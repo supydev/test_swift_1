60772f6090bb10276fcf30d12701d8a19d0d9554
supy <supy@supydeMacBook-Pro.local>
2015-07-11 00:15:05 +0800

    aa

---
+++ test_swift_1/test_swift_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_swift_1
+//
+//  Created by supy on 15/7/11.
+//  Copyright © 2015年 supy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
